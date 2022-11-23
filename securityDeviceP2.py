1554b3562ca4e01dfd9ad3d6debd114932cb7ef2
Ty <92275990+ty-dwyer@users.noreply.github.com>
2022-11-22 21:32:25 -0600

    Update securityDeviceP2.py

--- securityDeviceP2.py
+++ securityDeviceP2.py
@@ -1,18 +1,17 @@
-#Lock Check Method
 import random
 from time import sleep
 
 def LockCheck(key, LockStatus):
-    if key == 1 and LockStatus is True: #if unlock value is entered and lock is locked
+    if key == 1 and LockStatus is True:
         print("Unlocked")
         return False
 
-    elif key == 4 and LockStatus is False: #if lock value is entered and lock is unlocked
+    elif key == 4 and LockStatus is False:
         print("Locked")
         return True
 
     else:
-        if (key == 1) or (key == 4): #to ensure security
+        if (key == 1) or (key == 4):
             if LockStatus is True:
                 print("... Still Locked")
             else:
@@ -42,39 +41,36 @@ def DoorCheck(door):
     return door
 
 def main():
-    # Global Variables
     seconds = 0
     Code = random.randint(0,99999)
     print("Code is:",Code)
-    LockStatus = True # current status of lock
-    State = numDigits(Code) - 1  # Current state in FSM
+    LockStatus = True 
+    State = numDigits(Code) - 1 
     currState = State
 
     sleep(3)
-    #Main method
     print("Part 2")
-    while(True): # Part 2
+    while(True):
         
-        if DoorCheck(LockStatus) is False: #Check Lock
+        if DoorCheck(LockStatus) is False:
             print(seconds, "seconds")
             break
 
-        # enter value in one at a time
         val = random.randint(0,9)
         key = getDigit(Code, currState)
         seconds += 1
         print(" >> ", val)
 
-        if val == key: #if the the nth digit in key was match in sequence
+        if val == key:
             currState -= 1
-        else: #if you break the sequence
+        else:
             currState = State
 
-        if currState == -1: #if you reach end of code
+        if currState == -1:
             Value = random.randint(0,9)
             print("key >> ", Value)
             LockStatus = LockCheck(Value, LockStatus)
-            currState = State #reset state
+            currState = State
 
 
 if __name__ == "__main__":
